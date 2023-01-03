42957af2e286827c9ebd85619cf12bab6052efa5
zaouch-bahae-eddine <zaouch.bahae.eddine@gmail.com>
2023-01-03 15:12:59 +0100

    fix edit backend choice

--- choix_options_project/src/Controller/EtudiantController.php
+++ choix_options_project/src/Controller/EtudiantController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Bundle\FrameworkBundle\Controller\redirectToRoute;
 
 #[Route('/etudiant')]
 class EtudiantController extends AbstractController
@@ -28,13 +29,37 @@ class EtudiantController extends AbstractController
     #[Route('/choix_options/save', name: 'save_choice', methods: ['POST'])]
     public function add(Request $request, UeRepository $ueRepository, EntityManagerInterface $em, BlocRepository $blocRepository): Response
     {
+        $edit = false;
+        $user = $this->getUser();
+        $errors = [];
+        // choix enregistré dans la BD
         $currentChoice = array_map(function($e){
             return $e->getUe()->getId();
         },$this->getUser()->getChoices()->getValues());
 
-        $user = $this->getUser();
-        $errors = [];
+        if(count($currentChoice) > 0){
+            $edit = true;
+        }
+        //Choix envoyer
+        $ueChosed = $request->request->all('choices');
 
+        //All blocs
+        $blocs = array_map(function($e) {
+            return is_object($e) ? $e->getId() : 0;
+        }, $user->getPromotion()->getParcour()->getBlocs()->getValues());
+
+        //UE obligatoires
+        $ueMandatory =  $ueRepository->findBy(['bloc' => $blocs, 'status' => 2]);
+        $ueMandatoryIds = array_map(function($e) {
+            return is_object($e) ? $e->getId() : 0;
+        },$ueMandatory);
+
+        //Si le choix envoyer est deja enregistré dans la BD
+        if(!array_diff(array_values(array_diff($currentChoice, $ueChosed)), $ueMandatoryIds)){
+            return $this->redirectToRoute('etudiant_home', [], Response::HTTP_SEE_OTHER);
+        }
+
+        //Verification de la datelimite
         /*$now = new \DateTime();
         if(($user->getPromotion()->getDateLimiteChoixOptions()->format('U') - $now->format('U')) < 0 ){
             $errors[] = 'délai de choix est dépassé, veuillez contacter votre responsable de formation';
@@ -43,14 +68,14 @@ class EtudiantController extends AbstractController
                 'currentChoice' => $currentChoice,
             ]);
         }*/
-        foreach ($user->getChoices() as $choice){
-            $em->remove($choice);
-        }
-        $em->flush();
-        $blocs = array_map(function($e) {
-                return is_object($e) ? $e->getId() : 0;
-            }, $user->getPromotion()->getParcour()->getBlocs()->getValues());
 
+        //Delete current Choice to save a new choice
+//        foreach ($user->getChoices() as $choice){
+//            $em->remove($choice);
+//        }
+//        $em->flush();
+
+        // UE optionnelle possible
         $uesPossible = $ueRepository->findBy(['bloc' => $blocs, 'status' => 1]);
         $uesPossibleHash = [];
         $uesPossibleIds = array_map(function($e) {
@@ -59,8 +84,8 @@ class EtudiantController extends AbstractController
         foreach ($uesPossible as $ue){
             $uesPossibleHash[$ue->getId()] = $ue;
         }
-        $ueChosed = $request->request->all('choices');
-        if(($blocRepository->getNbUePossible($user->getPromotion()->getParcour()) != count($ueChosed)) || array_diff($uesPossibleIds, $ueChosed)){
+
+        if(($blocRepository->getNbUePossible($user->getPromotion()->getParcour()) != count($ueChosed)) || !in_array(array_diff($uesPossibleIds, $ueChosed), $uesPossibleIds)){
             $errors[] = 'les UEs choisi ne corespond pas à votre parcoure';
             return $this->render('etudiant/index.html.twig', [
                 'errors' => $errors,
@@ -79,7 +104,9 @@ class EtudiantController extends AbstractController
                 ]);
             } else {
                 $currentGroupe = round($ue->getCurrentCapacity() / $ue->getCapacityGroup()) + 1;
-                $ue->setCurrentCapacity($ue->getCurrentCapacity() + 1);
+                if(!$edit){
+                    $ue->setCurrentCapacity($ue->getCurrentCapacity() + 1);
+                }
             }
             $choice = (new Choice())
                 ->setUser($user)
@@ -88,7 +115,7 @@ class EtudiantController extends AbstractController
             $em->persist($choice);
         }
         //Ajouter les UEs obligatoires
-        foreach ($ueRepository->findBy(['bloc' => $blocs, 'status' => 2]) as $ue){
+        foreach ($ueMandatory as $ue){
             if($ue->getCurrentCapacity() == ($ue->getNbGroup()* $ue->getCapacityGroup())){
                 $errors[] = 'Capacité des groupe atteinte veillez contacter votre responsable de l\'année pour trouver une solution';
                 return $this->render('etudiant/index.html.twig', [
@@ -97,7 +124,9 @@ class EtudiantController extends AbstractController
                 ]);
             } else {
                 $currentGroupe = round($ue->getCurrentCapacity() / $ue->getCapacityGroup()) + 1;
-                $ue->setCurrentCapacity($ue->getCurrentCapacity() + 1);
+                if(!$edit){
+                    $ue->setCurrentCapacity($ue->getCurrentCapacity() + 1);
+                }
             }
             $choice = (new Choice())
                 ->setUser($user)
